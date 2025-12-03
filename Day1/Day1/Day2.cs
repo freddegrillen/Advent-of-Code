eee33a086a63101dafa021a500b00102da262d5a
Fredrik Grill <fredrik.grill@advania.se>
2025-12-03 14:44:48 +0100

    Day 2 part 1 done

---
+++ Day1/Day1/Day2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day1
+{
+    internal static class Day2
+    {
+        internal static void Execute()
+        {
+            Int64 result = 0;
+            string input = Console.ReadLine();
+            var strings = input.Split(',');
+            foreach ( var s in strings)
+            {
+                var stringSpan = s.Split("-");
+
+                Int64 from = Int64.Parse(stringSpan[0]);
+                Int64 to = Int64.Parse(stringSpan[1]);
+
+                for (Int64 i = from; i <= to; i++)
+                {
+                    var iString = i.ToString();
+                    if (iString.Length % 2 == 0)
+                    {
+                        int half = (iString.Length / 2);
+
+                        if(iString.Substring(0,half) == iString.Substring(half))
+                        {
+                            result += i;
+                        }
+
+                    }
+                }
+            }
+            Console.WriteLine(result);
+        }
+
+    }
+}
