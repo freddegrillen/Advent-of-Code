fb8c6b02c0ddaf34833010d36d5f508d2780d95a
Fredrik Grill <fredrik.grill@advania.se>
2025-12-03 14:16:17 +0100

    day 1 part 2 done

--- Day1/Day1/Program.cs
+++ Day1/Day1/Program.cs
@@ -8,25 +8,39 @@ string input = Console.ReadLine();
 while (input != "")
 {
     var change = int.Parse(input.Substring(1));
+    int fullRounds = (change - (change % 100)) / 100;
+    zeroCount += fullRounds;
+
     change = change % 100;
+
     if (input[0] == 'L')
     {
+        if (change >= current && current != 0)
+        {
+            zeroCount++;
+
+        }
         current -= change;
     }
     else
     {
+        if(change + current > 99)
+        {
+            zeroCount++;
+        }
         current += change;
     }
     AdaptCurrent();
     if(current == 0)
     {
-        zeroCount++;
-        Console.WriteLine($"zerocount is {zeroCount}. input: {input}");
+        //zeroCount++;
+        //Console.WriteLine($"zerocount is {zeroCount}. input: {input}");
     }
+    Console.WriteLine($"Current: {current} - Zerocount: {zeroCount}");
     input = Console.ReadLine();
 }
 
-Console.WriteLine($"Zerocount: {zeroCount}");
+Console.WriteLine($"Final Zerocount: {zeroCount}");
 Console.ReadLine();
 
 void AdaptCurrent()
