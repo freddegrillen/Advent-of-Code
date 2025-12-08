3a91e9bc542ff8a18754c78467b299dac76d3d71
Fredrik Grill <fredrik.grill@advania.se>
2025-12-08 16:51:34 +0100

    Day 5 part 1 done

---
+++ 2025/Day1/Day1/Day5.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day1
+{
+
+    internal class Range
+    {
+        internal Int64 from;
+        internal Int64 to;
+    }
+    internal static class Day5
+    {
+        internal static void Execute()
+        {
+            int freshCount = 0;
+            List<Range> ranges = new();
+            List<Int64> ingredientIds = new();
+
+
+            string input = Console.ReadLine();
+            while (input != "")
+            {
+                var nums = input.Split('-');
+                ranges.Add(new Range { from = Int64.Parse(nums[0]), to = Int64.Parse(nums[1]) });
+                input = Console.ReadLine();
+            }
+            input = Console.ReadLine();
+            while (input != "")
+            {
+                ingredientIds.Add(Int64.Parse(input));
+                input = Console.ReadLine();
+            }
+
+            var fresh = ingredientIds.Where(x => ranges.Where(r => r.from <= x && r.to >= x).Any()).ToList();
+            Console.WriteLine(fresh.Count);
+            
+        }
+    }    
+}
