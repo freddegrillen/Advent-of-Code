ff2e3661a6e7fe80b47a9e2a5b266b7156cbc0d7
Fredrik Grill <fredrik.grill@advania.se>
2025-12-15 11:08:20 +0100

    Day 7 part 1

---
+++ 2025/Day1/Day1/Day7.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day1
+{
+    internal static class Day7
+    {
+        public static void Execute()
+        {
+            List<string> rows = new();
+            int splitCount = 0;
+
+            string input = Console.ReadLine();
+            while (input != "")
+            {
+                rows.Add(input);
+                input = Console.ReadLine();
+            }
+
+            List<int> prevIndices = new();
+            prevIndices.Add(rows[0].IndexOf('S'));
+
+            for(int i = 1; i < rows.Count; i++)
+            {
+                List<int> provisional = prevIndices.ToList();
+                foreach (var index in prevIndices)
+                {
+                    
+                    if (rows[i][index] == '^')
+                    {
+                        provisional.Remove(index);
+                        provisional.Add(index - 1);
+                        provisional.Add(index + 1);
+                        splitCount++;
+                    }
+                }
+                prevIndices = provisional.Distinct().ToList();
+                Console.WriteLine($"Row: {i} splitcount: {splitCount}");
+            }
+            Console.WriteLine(splitCount);
+
+        }
+
+    }
+}
--- 2025/Day1/Day1/Program.cs
+++ 2025/Day1/Day1/Program.cs
@@ -3,4 +3,4 @@ using Day1;
 
 //Day1.Day1.Execute();
 //Day2.Execute();
-Day6.Execute();
+Day7.Execute();
